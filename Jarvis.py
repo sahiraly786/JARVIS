cd87d90747df3c722f302cc26e434351ad5e824d
Sahir <sahiraly786@gmail.com>
2026-06-03 21:10:59 +0500

    Update Jarvis.py

--- Jarvis.py
+++ Jarvis.py
@@ -11,7 +11,8 @@ HOW IT WORKS:
 
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-import webbrowser, datetime, os, sys, requests
+# import webbrowser
+, datetime, os, sys, requests
 
 
 # ── Load API keys from .env ──
